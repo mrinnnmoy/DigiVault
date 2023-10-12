4c2d71388777d7abd8d4008e6e3706f953cca906
mrinnnmoy <mrinmoy202000@gmail.com>
2023-10-12 17:16:24 +0530

    build-routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import User from './components/User';
 import About from './components/About';
 import Features from './components/Features';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
-      <User />
-      <About />
-      <Features />
-      <Contact />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,39 @@
 import React from 'react';
 import homeImg from '../assets/home-img.svg';
+import User from './User';
+import About from './About';
+import Features from './Features';
+import Contact from './Contact';
 
 const Home = () => {
     return (
-        <home>
-            {/* Home section, Left-side */}
-            <div className='home-left'>
-                <div className='home-left-top'>
-                    <span>Easily Share & Store files online.</span>
+        <>
+            <home>
+                {/* Home section, Left-side */}
+                <div className='home-left'>
+                    <div className='home-left-top'>
+                        <span>Easily Share & Store files online.</span>
+                    </div>
+                    <div className='home-left-middle'>
+                        <span>A Decentralized file storage and sharing solution for everyone using IPFS.</span>
+                    </div>
+                    <div className='home-left-bottom launch-btn'>
+                        <a href="/dashboard" rel='noreferrer'>
+                            <button>Launch App</button>
+                        </a>
+                    </div>
                 </div>
-                <div className='home-left-middle'>
-                    <span>A Decentralized file storage and sharing solution for everyone using IPFS.</span>
-                </div>
-                <div className='home-left-bottom launch-btn'>
-                    <button href="./">Launch App</button>
-                </div>
-            </div>
 
-            {/* Home section. right side */}
-            <div className='home-right'>
-                <img src={homeImg} alt='home-img' />
-            </div>
-        </home>
+                {/* Home section. right side */}
+                <div className='home-right'>
+                    <img src={homeImg} alt='home-img' />
+                </div>
+            </home>
+            <User />
+            <About />
+            <Features />
+            <Contact />
+        </>
     )
 };
 
