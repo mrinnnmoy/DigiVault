496ae132f2ac961d5124288e7897b60f6b3ece42
mrinnnmoy <mrinmoy202000@gmail.com>
2024-01-29 18:27:15 +0530

    Rebuild-displayImg

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Display = () => {
+const Display = ({ contract, account }) => {
+  const [data, setData] = useState("");
+  const getData = async () => {
+    let dataArray;
+    const Otheraddress = document.querySelector(".address").value;
+    try {
+      if (Otheraddress) {
+        dataArray = await contract.display(Otheraddress);
+        console.log(dataArray);
+      } else {
+        dataArray = await contract.display(account);
+      }
+    } catch (error) {
+      alert("Access Denied.");
+    }
+    const isEmpty = Object.keys(dataArray).length === 0;
+
+    if (!isEmpty) {
+      const str = dataArray.toString();
+      const str_array = str.split(",");
+
+      console.log(str);
+      console.log(str_array);
+
+      const images = str_array.map((item, i) => {
+        return (
+          <a href={item} key={i} target='_blank' rel='noreferrer'>
+            <img key={i} src={`https://gateway.pinata.cloud/ipfs/${item.substring(6)}`} alt="new" className='image-list' />
+          </a>
+        );
+      });
+      setData(images);
+    } else {
+      alert("No images to display.");
+    }
+  };
+
   return (
-    <div>Display</div>
+    <>
+      <div className="">{data}</div>
+      <input type="text" placeholder='Enter Address' className='address' />
+      <button className="" onClick={getData}>Get Data</button>
+    </>
   )
 };
 
